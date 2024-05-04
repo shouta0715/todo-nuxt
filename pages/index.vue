e26dc9f4f0b21d0e971ffda00947101e0e029414
shouta0715 <Myheroes0715@gmail.com>
2024-05-04 14:54:08 +0900

    add: tailwind css

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-bold text-center text-blue-500">Home Page</h1>
+    <p>Click <nuxt-link to="/">here</nuxt-link> to go to the about page</p>
+  </div>
+</template>
